6572d506c4655a39f0dd804dc8d06963c8f3532e
Esswar <esswarv@gmail.com>
2022-02-01 16:27:44 -0600

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const item_number = 8
+
+if (item_number >=10) {
+    console.log ("entered large item number: " + item_number)
+ } else {
+    console.log ("entered small item number:" + item_number)
+ }
+
+ console.log ("Hello World!!!!")
